2645bcf2d0e8d2ff6a61b69c589b8c39e6632465
Lokesh-Kanna <lokeshkanna.work@gmail.com>
2021-11-28 15:28:08 +0530

    Added formik and yup libraries and added validation

--- src/AddContacts.js
+++ src/AddContacts.js
@@ -1,9 +1,10 @@
 import { NavBar } from "./NavBar";
 import Button from "@mui/material/Button";
 import ControlPointIcon from "@mui/icons-material/ControlPoint";
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import TextField from "@mui/material/TextField";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 export function AddContacts() {
   const navigate = useNavigate();
@@ -22,90 +23,162 @@ export function AddContacts() {
     </div>
   );
 }
+
 export function AddContactPage({ setCon }) {
   const navigate = useNavigate();
 
-  const [name, setName] = useState();
-  const [contactNo, setContactNo] = useState();
-  const [mail, setMail] = useState();
-  const [street, setStreet] = useState();
-  const [city, setCity] = useState();
-  const [country, setCountry] = useState();
-  const [zip, setZip] = useState();
-  const [photo, setPhoto] = useState();
+  // const validateForm = (values) => {
+  //   console.log("Validation", values);
+  //   const errors = {};
+
+  //   if (values.phone.length < 10) {
+  //     errors.phone = "Invalid phone number";
+  //   }
+
+  //   if (values.email.length < 2) {
+  //     errors.email = "Invalid Email";
+  //   } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)) {
+  //     errors.email = "Invalid Email";
+  //   }
+  //   return errors;
+  // };
+
+  const formValidationSchema = yup.object({
+    phone: yup
+      .string()
+      .min(10, "This doesnt seem to be a valid phone number 🤔")
+      .required(
+        "It is only logical to have a phone number in a contact list don't you think? 😅"
+      ),
+    email: yup
+      .string()
+      .matches(
+        /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+        "This doesnt seem to be a valid email ID 🤔"
+      )
+      .required(
+        "We will need a mail ID one day or the other, so why not add it now? 🤷‍♀️"
+      ),
+  });
+
+  const { handleChange, handleBlur, handleSubmit, values, errors, touched } =
+    useFormik({
+      initialValues: {
+        name: "",
+        phone: "",
+        email: "",
+        street: "",
+        city: "",
+        country: "",
+        zip: "",
+        url: "",
+      },
+      // validate={validateForm}
+      validationSchema: formValidationSchema,
+      onSubmit: (values) => {
+        setCon(values);
+        navigate("/");
+      },
+    });
+
   return (
     <div>
       <NavBar />
-      <div className="form-div">
+      <form className="form-div" onSubmit={handleSubmit}>
         <TextField
           className="standard-basic"
           label="Full Name"
           variant="filled"
-          onChange={(event) => setName(event.target.value)}
+          id="name"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
         <TextField
           className="standard-basic"
           label="Phone No."
           variant="filled"
-          onChange={(event) => setContactNo(event.target.value)}
+          id="phone"
+          name="phone"
+          value={values.phone}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
+        {errors.phone && touched.phone && errors.phone}
         <TextField
           className="standard-basic"
           label="Mail ID"
           variant="filled"
-          onChange={(event) => setMail(event.target.value)}
+          id="email"
+          name="email"
+          value={values.email}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
+        {errors.email && touched.email && errors.email}
         <TextField
           className="standard-basic"
           label="Street"
           variant="filled"
-          onChange={(event) => setStreet(event.target.value)}
+          id="street"
+          name="street"
+          value={values.street}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
         <TextField
           className="standard-basic"
           label="City"
           variant="filled"
-          onChange={(event) => setCity(event.target.value)}
+          id="city"
+          name="city"
+          value={values.city}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
         <TextField
           className="standard-basic"
           label="Country"
           variant="filled"
-          onChange={(event) => setCountry(event.target.value)}
+          id="country"
+          name="country"
+          value={values.cpuntry}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
         <TextField
           className="standard-basic"
           label="Zip Code"
           variant="filled"
-          onChange={(event) => setZip(event.target.value)}
+          id="zip"
+          name="zip"
+          value={values.zip}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
         <TextField
           className="standard-basic"
           label="Photo URL"
           variant="filled"
-          onChange={(event) => setPhoto(event.target.value)}
+          input
+          id="url"
+          name="url"
+          value={values.url}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
         <Button
           className="buttn"
           variant="contained"
-          onClick={() => {
-            const newCont = {
-              name,
-              contactNo,
-              mail,
-              street,
-              city,
-              country,
-              zip,
-              photo,
-            };
-            setCon(newCont);
-            navigate("/");
-          }}
+          type="submit"
+          // onClick={() => {
+          //
+          // }}
         >
           <div className="addbtn">Save Contact</div>
         </Button>
-      </div>
+      </form>
     </div>
   );
 }
